d5f26a1872af1726e70b3b2b94aceeebdc1bf6e2
Ty Hill <crazysnoheadman@yahoo.com>
2016-01-04 11:56:49 -0700

    completed assignment

---
+++ js/app.js
@@ -0,0 +1,23 @@
+var app = angular.module('miniRouting', ['ui.router']);
+
+app.config(function($stateProvider, $urlRouterProvider) {
+  $stateProvider
+    .state('home', {
+      templateUrl: 'js/home/homeTmpl.html',
+      controller: 'js/home/homeCtrl.js',
+      url: '/'
+    })
+    .state('settings', {
+      templateUrl: 'js/settings/settingsTmpl.html',
+      controller: 'settingsCtrl',
+      url: '/settings'
+    })
+    .state('products', {
+      templateUrl: 'js/products/productTmpl.html',
+      controller: 'productsCtrl',
+      url: '/products/:id'
+    });
+
+  $urlRouterProvider
+    .otherwise('/');
+});
